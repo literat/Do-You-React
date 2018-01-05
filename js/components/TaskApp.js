1504f12c8bff647ba3f779c8218480c43d8b5b69
literat <tomas@litera.me>
2018-01-05 16:20:33 +0100

    Episode #6 - Done task and deleting tasks

--- js/components/TaskApp.js
+++ js/components/TaskApp.js
@@ -1,16 +1,36 @@
 var TaskApp = React.createClass({
     getInitialState: function() {
         return {
-            items: [],
+            itemsToDo: [],
+            itemsDone: [],
             task: ''
         };
     },
 
     addTask: function(e) {
-        var items = this.state.items.concat([this.state.task]);
-        var task = '';
-        this.setState({ items, task });
+        var task = this.state.task;
+        if(task != '') {
+            this.setState({
+                itemsToDo: this.state.itemsToDo.concat([this.state.task]),
+                task: ''
+            });
+        }
+
         e.preventDefault();
+    },
+
+    doneTask: function(item) {
+        var itemsToDo = this.state.itemsToDo;
+        var trans = itemsToDo[item];
+        delete itemsToDo[item];
+        var itemsDone = this.state.itemsDone.concat(trans);
+        this.setState({ itemsToDo, itemsDone });
+    },
+
+    deleteTask: function (item) {
+        var items = this.state.itemsToDo;
+        delete items[item];
+        this.setState({ items });
     },
 
     onChange: function(e) {
@@ -23,7 +43,8 @@ var TaskApp = React.createClass({
         return (
             <div>
                 <h1>My Tasks</h1>
-                <TaskList items={this.state.items} />
+                <TaskListDone items={this.state.itemsDone} />
+                <TaskList items={this.state.itemsToDo} delete={this.deleteTask} done={this.doneTask} />
 
                 <form onSubmit={this.addTask}>
                     <input onChange={this.onChange} value={this.state.task} />
--- js/components/TaskList.js
+++ js/components/TaskList.js
@@ -1,11 +1,26 @@
 var TaskList = React.createClass({
 
-    render: function() {
-        var displayTask = (task) => <li>{task}</li>;
+    done: function(item) {
+        this.props.done(item);
+    },
 
+    delete: function(item) {
+        this.props.delete(item);
+    },
+
+    displayTask: function(task, item){
         return (
-            <ul>
-                { this.props.items.map(displayTask) }
+            <li>
+                <span onClick={this.done.bind(this, item)} >{task}</span>
+                <span onClick={this.delete.bind(this, item)} > [X]</span>
+            </li>
+        );
+    },
+
+    render: function() {
+        return (
+            <ul onClick={this.handleClick}>
+                { this.props.items.map(this.displayTask) }
             </ul>
         );
     }
