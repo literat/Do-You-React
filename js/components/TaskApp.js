a7c9df7cab21edb089e851e6c211ad170aeefa6a
literat <tomas@litera.me>
2018-01-05 15:57:22 +0100

    Episode #6 - Adding tasks

---
+++ js/components/TaskApp.js
@@ -0,0 +1,38 @@
+var TaskApp = React.createClass({
+    getInitialState: function() {
+        return {
+            items: [],
+            task: ''
+        };
+    },
+
+    addTask: function(e) {
+        var items = this.state.items.concat([this.state.task]);
+        var task = '';
+        this.setState({ items, task });
+        e.preventDefault();
+    },
+
+    onChange: function(e) {
+        var task = e.target.value;
+
+        this.setState({ task });
+    },
+
+    render: function() {
+        return (
+            <div>
+                <h1>My Tasks</h1>
+                <TaskList items={this.state.items} />
+
+                <form onSubmit={this.addTask}>
+                    <input onChange={this.onChange} value={this.state.task} />
+                    <button>Add Task</button>
+                </form>
+            </div>
+        );
+    }
+
+});
+
+React.render(<TaskApp />, document.body);
